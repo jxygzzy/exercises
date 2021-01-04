c02209dd8e89b1c3f58dea0a4694f6a5ca034bf9
zzy <jxygzzy@163.com>
2021-01-04 20:55:23 +0800

    2021-01-04

--- src/打印大小写字母/Print.java
+++ src/打印大小写字母/Print.java
@@ -1,28 +1,29 @@
 package 打印大小写字母;
 
-public class Print implements InterfaceA,InterfaceB{
+public class Print implements InterfaceA, InterfaceB {
     int N;
+
     @Override
     public void setN(int n) {
-        this.N=n;
+        this.N = n;
     }
 
     @Override
     public void printCapitalLetter() {
-        int n=N%26;
-        if(N%26==0&&N!=0) n=26;
+        int n = N % 26;
+        if (N % 26 == 0 && N != 0) n = 26;
         for (int i = 0; i < n; i++) {
-            System.out.printf("%c",('A'+i));
+            System.out.printf("%c", ('A' + i));
         }
         System.out.println();
     }
 
     @Override
     public void printLowercaseLetter(int n) {
-        if(n%26==0&&n!=0) n=26;
-        else n=n%26;
+        if (n % 26 == 0 && n != 0) n = 26;
+        else n = n % 26;
         for (int i = 0; i < n; i++) {
-            System.out.printf("%c",('a'+i));
+            System.out.printf("%c", ('a' + i));
         }
         System.out.println();
     }
